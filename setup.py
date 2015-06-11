d5bcad030df3c55ea9bacd1df037501eaac8a529
tetsuwo <tetsuwo.oishi@gmail.com>
2015-06-11 23:26:07 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 import sys
 
 sys.path.append('./src')
 sys.path.append('./tests')
 
 setup(
-    name = 'IROLiNK',
-    version = '0.1',
+    name='IROLiNK',
+    version='0.1',
     description='This is test codes for travis ci',
-    packages = find_packages(),
-    test_suite = 'irolink_test.suite'
+    packages=find_packages(),
+    test_suite='irolink_test.suite'
 )
