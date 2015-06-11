c08a7e184a330a336459422e6b7ddcd31fe5b2d3
tetsuwo <tetsuwo.oishi@gmail.com>
2015-06-11 23:11:45 +0900

    Update irolink.py

--- src/irolink.py
+++ src/irolink.py
@@ -2,7 +2,9 @@ from flask import abort
 from flask import Flask
 from flask import redirect
 from flask import render_template
+import colorsys
 import re
+import sys
 
 app = Flask(__name__, static_url_path='')
 
@@ -20,51 +22,6 @@ def show_test():
     return render_template('test.html')
 
 
-class ColorConvertUtil(object):
-    @staticmethod
-    def rgbstr_to_rgbhex(code):
-        ret = {
-            'r': code[0:2],
-            'g': code[2:4],
-            'b': code[4:6]
-        }
-        return ret
-
-    @staticmethod
-    def rgbhex_to_rgbdec(r, g, b):
-        ret = {
-            'r': int(r, 16),
-            'g': int(g, 16),
-            'b': int(b, 16)
-        }
-        return ret
-
-    @staticmethod
-    def rgbdec_to_rgbpercent(r, g, b):
-        ret = {
-            'r': float(r) / 256 * 100,
-            'g': float(g) / 256 * 100,
-            'b': float(b) / 256 * 100
-        }
-        return ret
-
-    @staticmethod
-    def rgbdec_to_cmyk(r, g, b):
-        return "hoge"
-        if (r == 0) and (g == 0) and (b == 0):
-            return {'c': 0, 'm': 0, 'y': 0, 'k': 100}
-        c = 1 - r / 255
-        m = 1 - g / 255
-        y = 1 - b / 255
-        min_cmy = min(c, m, y)
-        c = (c - min_cmy) / (1 - min_cmy)
-        m = (m - min_cmy) / (1 - min_cmy)
-        y = (y - min_cmy) / (1 - min_cmy)
-        k = min_cmy
-        ret = {'c': c*100, 'm': m*100, 'y': y*100, 'k': k*100}
-        return ret
-
-
 @app.route('/rgb-hex/<code>')
 def color_detail_rgb_hex(code):
     if not re.match(r'^([0-9a-zA-Z]){6}$', code):
@@ -78,8 +35,10 @@ def color_detail_rgb_hex(code):
         rgb_dec['r'], rgb_dec['g'], rgb_dec['b'])
     cmyk = ColorConvertUtil.rgbdec_to_cmyk(
         rgb_dec['r'], rgb_dec['g'], rgb_dec['b'])
-    return "HEX %s / DEC %s / PERCENT %s / CMYK %s" % (
-        rgb_hex, rgb_dec, rgb_percent, cmyk)
+    raw_hls = colorsys.rgb_to_hls(rgb_dec['r'], rgb_dec['g'], rgb_dec['b'])
+    hsl = {'h': raw_hls[0], 's': raw_hls[2], 'l': raw_hls[1]}
+    raw_hsv = colorsys.rgb_to_hsv(rgb_dec['r'], rgb_dec['g'], rgb_dec['b'])
+    hsv = {'h': raw_hsv[0], 's': raw_hsv[1], 'v': raw_hsv[2]}
     colorcode_text = '#' + code
     colorcode_link = code
     return render_template(
@@ -89,7 +48,9 @@ def color_detail_rgb_hex(code):
         rgb_hex=rgb_hex,
         rgb_dec=rgb_dec,
         rgb_percent=rgb_percent,
-        cmyk=cmyk
+        cmyk=cmyk,
+        hsl=hsl,
+        hsv=hsv
     )
 
 
@@ -137,6 +98,49 @@ def send_robots():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('errors/404.html'), 404
+
+
+class ColorConvertUtil(object):
+    @staticmethod
+    def rgbstr_to_rgbhex(code):
+        ret = {
+            'r': code[0:2],
+            'g': code[2:4],
+            'b': code[4:6]
+        }
+        return ret
+
+    @staticmethod
+    def rgbhex_to_rgbdec(r, g, b):
+        ret = {
+            'r': int(r, 16),
+            'g': int(g, 16),
+            'b': int(b, 16)
+        }
+        return ret
+
+    @staticmethod
+    def rgbdec_to_rgbpercent(r, g, b):
+        ret = {
+            'r': float(r) / 256 * 100,
+            'g': float(g) / 256 * 100,
+            'b': float(b) / 256 * 100
+        }
+        return ret
+
+    @staticmethod
+    def rgbdec_to_cmyk(r, g, b):
+        if (r == 0) and (g == 0) and (b == 0):
+            return {'c': 0, 'm': 0, 'y': 0, 'k': 100}
+        c = 1 - r / float(255)
+        m = 1 - g / float(255)
+        y = 1 - b / float(255)
+        min_cmy = min(c, m, y)
+        c = (c - min_cmy) / (1 - min_cmy)
+        m = (m - min_cmy) / (1 - min_cmy)
+        y = (y - min_cmy) / (1 - min_cmy)
+        k = min_cmy
+        return {'c': c*100, 'm': m*100, 'y': y*100, 'k': k*100}
 
 
 if __name__ == "__main__":
