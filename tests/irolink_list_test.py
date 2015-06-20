9e7b98de663db8b57b3fe66a62a930feb7c3a6e1
tetsuwo <tetsuwo.oishi@gmail.com>
2015-06-20 09:17:08 +0900

    Add test codes

---
+++ tests/irolink_list_test.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import unittest
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../src/')
+import irolink
+
+
+class IROLiNKListTest(unittest.TestCase):
+    def setUp(self):
+        irolink.app.debug = False
+        self.app = irolink.app.test_client()
+
+    def test_show_html16color_01(self):
+        raw_response = self.app.get(
+            '/html-16-base-colors'
+        )
+        assert raw_response.status_code == 200
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTests(unittest.makeSuite(IROLiNKListTest))
+    return suite
